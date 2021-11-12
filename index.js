f9ef8504fad8f4b1fda78561dc18369ff19e797f
Jose <zaragoza.jose@gmail.com>
2021-11-12 01:15:47 -0800

    low population

--- index.js
+++ index.js
@@ -91,10 +91,17 @@ const zooAnimals = [
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowPopulationAnimals(array){
+    const filtered = array.filter(function(item){
+      if(item.population < 5){
+        return item.animal_name
+        
+      }
+    })
+    return filtered;
   }
   
+  console.log(lowPopulationAnimals(zooAnimals))
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
   The zoo needs to know their total animal population across the United States. 
